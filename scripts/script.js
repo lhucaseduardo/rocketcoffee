ec8f76d37ba3142292dc513f6dd37f78041eeec3
lhucaseduardo <lhucasedu@gmail.com>
2022-07-01 23:11:36 -0300

    criação do menu burger

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function showMenuOn(){
+    document.getElementById("burguerOpen").style.display = "none";
+    document.getElementById("burguerClosed").style.display = "block";
+}
+
+function showMenuOff(){
+    document.getElementById("burguerOpen").style.display = "block";
+    document.getElementById("burguerClosed").style.display = "none";
+}
