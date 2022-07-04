70dfc5bbaa8d338e20a5fd45f9e4d4eecda5e5db
lhucaseduardo <lhucasedu@gmail.com>
2022-07-04 14:44:23 -0300

    ajuste de responsividade no menu burger

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,39 @@
+const burgerOpenElement = document.getElementById("burguerOpen");
+const burgerClosedElement = document.getElementById("burguerClosed");
+let isMenuOpened = false;
+
 function showMenuOn(){
-    document.getElementById("burguerOpen").style.display = "none";
-    document.getElementById("burguerClosed").style.display = "block";
+    burgerOpenElement.style.display = "none";
+    burgerClosedElement.style.display = "block";
+    isMenuOpened = true;   
 }
 
 function showMenuOff(){
-    document.getElementById("burguerOpen").style.display = "block";
-    document.getElementById("burguerClosed").style.display = "none";
+    burgerOpenElement.style.display = "block";
+    burgerClosedElement.style.display = "none";
+    isMenuOpened = false;
 }
+
+// Listener to handle with resize and hamburger menu button
+window.addEventListener('resize', () => {
+    const currentScreenWidth = document.body.clientWidth;
+    const isBiggerScreen = currentScreenWidth > 997;
+    const isTabletScreen = currentScreenWidth <= 997;
+    
+    if (isBiggerScreen) {
+        isMenuOpened = false;
+        burgerOpenElement.style.display = "none";
+        burgerClosedElement.style.display = "none";
+    }
+    
+    if (isTabletScreen) {
+        if (isMenuOpened) {
+            burgerOpenElement.style.display = "none";
+            burgerClosedElement.style.display = "block";    
+            return;
+        } else {
+            burgerOpenElement.style.display = "block";
+            burgerClosedElement.style.display = "none";
+        }
+    }
+});
