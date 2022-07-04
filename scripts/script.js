1681bbb8f21e01900b4bf50872e9bbdb5713573e
lhucaseduardo <lhucasedu@gmail.com>
2022-07-04 17:07:01 -0300

    finalização do menu burger e responsividade

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
 const burgerOpenElement = document.getElementById("burguerOpen");
 const burgerClosedElement = document.getElementById("burguerClosed");
+const menuMobile = document.getElementById("menuList");
+const websiteContent = document.getElementById("siteContent")
 let isMenuOpened = false;
 
 function showMenuOn(){
     burgerOpenElement.style.display = "none";
     burgerClosedElement.style.display = "block";
+    menuMobile.style.display = "flex"
+    websiteContent.style.display = "none"
     isMenuOpened = true;   
 }
 
 function showMenuOff(){
     burgerOpenElement.style.display = "block";
     burgerClosedElement.style.display = "none";
+    menuMobile.style.display = "none"
+    websiteContent.style.display = "block"
     isMenuOpened = false;
 }
 
@@ -24,16 +30,21 @@ window.addEventListener('resize', () => {
         isMenuOpened = false;
         burgerOpenElement.style.display = "none";
         burgerClosedElement.style.display = "none";
+        websiteContent.style.display = "block"
     }
     
     if (isTabletScreen) {
         if (isMenuOpened) {
             burgerOpenElement.style.display = "none";
-            burgerClosedElement.style.display = "block";    
+            burgerClosedElement.style.display = "block";  
+            menuMobile.style.display = "flex"  
+            websiteContent.style.display = "none"
             return;
         } else {
             burgerOpenElement.style.display = "block";
             burgerClosedElement.style.display = "none";
+            menuMobile.style.display = "none"  
+            websiteContent.style.display = "block"
         }
     }
 });
